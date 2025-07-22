cecaec264f329600da642ef96028a59311fd5b63
ghinanurul <ghinanurul31@gmail.com>
2025-07-22 21:20:47 +0700

    Initial commit for Vercel

--- api/index.php
+++ api/index.php
@@ -6,10 +6,8 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$request = Illuminate\Http\Request::capture();
-
-$response = $kernel->handle($request);
-
-$response->send();
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
 
 $kernel->terminate($request, $response);
