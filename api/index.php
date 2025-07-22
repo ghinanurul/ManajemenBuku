cd24ecac1d2a6b9bb164c51bad0792f751ce985b
ghinanurul <ghinanurul31@gmail.com>
2025-07-22 22:29:35 +0700

    Fix Deploy

--- api/index.php
+++ api/index.php
@@ -6,8 +6,7 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request)->send();
 
 $kernel->terminate($request, $response);
